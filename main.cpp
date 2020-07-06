1f70b1a20ee4e286ee56458c0373cf39f4411d32
anchal2068 <67888376+anchal2068@users.noreply.github.com>
2020-07-06 10:10:42 +0530

    product.c
    This file prints the product of two numbers entered by the user.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double firstnum, secondnum,productoftwonumbers;
+    cout << "Enter two numbers:" ;
+    cin >> firstnum >> secondnum;
+    productoftwonumbers=firstnum*secondnum;
+    cout<<"Product="<<productoftwonumbers;
+
+    return 0;
+}
